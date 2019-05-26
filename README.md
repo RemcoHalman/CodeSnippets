c056530db092e6a2143e39bec9dd6028c2f11530
Remco Halman <remco.halman@gmail.com>
2019-05-26 19:58:48 +0200

    Added PyQT .ui converter

--- README.md
+++ README.md
@@ -16,13 +16,13 @@
   - Copy's all images and resizes them to set basewidth in script.
 - `watermark.py`
   - Set a watermark in bottom right corner(dont make your watermark to big)
+- `ui_converter.py`
+  - Converting QtDesigner UI file to .py file. Place and use in the same directory as the `.ui` file
 
 ---
 
 #### Batch adding watermark for images (watermark.py)
-
 ###### Free to use But use it at your own risk!
-
 - Logging in the same location as the python script
 - Paths to set:
   - Input Folder
---
+++ small_scripts/ui_converter.py
@@ -0,0 +1,48 @@
+# Use this script to convert Qt Designer UI and resource files to python
+import os
+import subprocess
+
+input_path = os.path.join(os.path.dirname(__file__))  # "MainWindowTest", "ui"
+output_path = os.path.join(os.path.dirname(__file__))  # "src", "widgets"
+
+
+def check_file(file):
+    if "All changes made in this file will be lost" in open(file).read():
+        return True
+    return False
+
+
+for f in os.listdir(input_path):
+    if not os.path.isfile(f):
+        pass
+
+    file_name, extension = os.path.splitext(f)
+
+    if extension == ".ui":
+        input_file = os.path.join(input_path, f)
+        output_file = os.path.join(output_path, file_name + ".py")
+        if os.path.isfile(output_file):
+            if not check_file(output_file):
+                print(
+                    "Warning: tried to overwrite a file generated outside Qt Creator. {}".format(
+                        output_file
+                    )
+                )
+                continue
+        subprocess.call(
+            "pyuic5 --import-from=widgets -x {} -o {}".format(input_file, output_file),
+            shell=True,
+        )
+    elif extension == ".qrc":
+        input_file = os.path.join(input_path, f)
+        output_file = os.path.join(output_path, file_name + "_rc.py")
+        if os.path.isfile(output_file):
+            if not check_file(output_file):
+                print(
+                    "Warning: tried to overwrite a file generated outside Qt Creator. {}".format(
+                        output_file
+                    )
+                )
+                continue
+        subprocess.call("pyrcc5 {} -o {}".format(input_file, output_file), shell=True)
+
